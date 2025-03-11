15a9afe9b17e39f7a5d52fbd69a4d9461dad919d
gg300 <georgebot300@yahoo.com>
2025-03-11 22:08:26 +0200

    modified footer

--- geo-web/src/app/Components/footer/footer.component.ts
+++ geo-web/src/app/Components/footer/footer.component.ts
@@ -9,5 +9,19 @@ import {CommonModule} from '@angular/common';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+footer: {name:string,link?:string}[]=[
+  {name:'contact',
+  },
+  {name:'Linkedin',
+    link:'https://www.linkedin.com/in/george-bot-49b755258/',
+  },
+  {name:'Facebook',
+    link:'https://www.facebook.com/profile.php?id=100009534380212',
+  },
+  {name:'Github',
+    link:'https://github.com/gg300',
+  },
+  {name:'contact',
+  },
+];
 }
