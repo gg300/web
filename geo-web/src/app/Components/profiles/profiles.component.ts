2af2491a67e5885dab6c8d372d8fda841bb10258
gg300 <georgebot300@yahoo.com>
2025-03-16 17:44:11 +0200

    plm

--- geo-web/src/app/Components/profiles/profiles.component.ts
+++ geo-web/src/app/Components/profiles/profiles.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import {NgForOf} from '@angular/common';
 @Component({
   selector: 'app-profiles',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './profiles.component.html',
   styleUrl: './profiles.component.css'
 })
 export class ProfilesComponent {
-
+texts : string[]=[
+  'asfsda dsauh duioaj odj oifjasoi fjoaspf opasj fposaj pofjaspo jfapoj sfposj opifjsdpo jfpao jpoaijf poaij' ,
+  '          do ijnaoid oansd oa iodjasio jdsaoi jsaoi jaoid jao djoisa joa jaos jaoji aoid jao disaj oidaj oa iaj doasj djia s' ,
+  '          dijaopdsjoi ajd sa asfsda dsauh duioaj odj oifjasoi fjoaspf opasj fposaj pofjaspo jfapoj sfposj opifjsdpo jfpao jpoaijf poaij' ,
+  '          do ijnaoid oansd oa iodjasio jdsaoi jsaoi jaoid jao djoisa joa jaos jaoji  aoid jao disaj oidaj oa iaj doasj djia s' ,
+  '          asfsda dsauh duioaj odj oifjasoi fjoaspf opasj fposaj pofjaspo jfapoj sfposj opifjsdpo jfpao jpoaijf poaij' ,
+  '          do ijnaoid oansd oa iodjasio jdsaoi jsaoi jaoid jao djoisa joa jaos jaoji  aoid jao disaj oidaj oa iaj doasj djia s' ,
+  '          dijaopdsjoi ajd saasfsda dsauh duioaj odj oifjasoi fjoaspf opasj fposaj pofjaspo jfapoj sfposj opifjsdpo jfpao jpoaijf poaij' ,
+  '          do ijnaoid oansd oa iodjasio jdsaoi jsaoi jaoid jao djoisa joa jaos jaoji  aoid jao disaj oidaj oa iaj doasj djia s' ,
+  '          dijaopdsjoi ajd saasfsda dsauh duioaj odj oifjasoi fjoaspf opasj fposaj pofjaspo jfapoj sfposj opifjsdpo jfpao jpoaijf poaij' ,
+  '          do ijnaoid oansd oa iodjasio jdsaoi jsaoi jaoid jao djoisa joa jaos jaoji  aoid jao disaj oidaj oa iaj doasj djia s' ,
+  '          dijaopdsjoi ajd saasfsda dsauh duioaj odj oifjasoi fjoaspf opasj fposaj pofjaspo jfapoj sfposj opifjsdpo jfpao jpoaijf poaij' ,
+  '          do ijnaoid oansd oa iodjasio jdsaoi jsaoi jaoid jao djoisa joa jaos jaoji  aoid jao disaj oidaj oa iaj doasj djia s',
+]
 }
