14356be790c047b2ee5647f345fab0b76126820c
gg300 <georgebot300@yahoo.com>
2025-03-18 22:36:29 +0200

    plm

--- geo-web/src/app/Components/experiences/experiences.component.ts
+++ geo-web/src/app/Components/experiences/experiences.component.ts
@@ -11,12 +11,20 @@ import {CommonModule} from '@angular/common';
 export class ExperiencesComponent {
   experiences:{image:string,description:string}[] = [
     {
-      image:'',
-      description:'experience'
+      image:'https://media.licdn.com/dms/image/v2/C4E0BAQGGySdJKRLMRA/company-logo_200_200/company-logo_200_200/0/1631336479763?e=1747872000&v=beta&t=uugYqbb5MWGLKW1Ywv3hPELnPbYVifcbViQ6PKYs2_4',
+      description:'Verindyen Intern'
     },
     {
-      image:'',
-      description:'experience'
-    }
+      image:'https://lh3.googleusercontent.com/p/AF1QipMKApAm-lT5IVRPvU-xgu6Y2lDGRc2YRj9cQc-h=s1360-w1360-h1020',
+      description:'Liga AC Volunteer'
+    },
+    {
+    image:'https://media.licdn.com/dms/image/v2/D4D0BAQEbZxy1DD58gA/company-logo_200_200/company-logo_200_200/0/1699530411284/therapime_logo?e=1747872000&v=beta&t=WnVwLUIAO1HNmS5nBMwE3nUYKLMD5AAY-N4K6wGuzD4',
+  description:'Front End at Therapi'
+    },
+    {
+      image:'public/assets/images/regina.png',
+      description:'whatever'
+    },
   ];
 }
